2f36fbdebc75e24c50f73089ada1c1eb8946ef72
Kim Jong-heon <maxmirjh@gmail.com>
2017-09-13 12:51:04 +0900

    스프링부트 초기 생성

---
+++ src/main/java/com/example/demo/ArrangeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrangeApplication.class, args);
+	}
+}
